5495b89d50a820b66df5a25515cc2ba068c2db21
Manobendra <ghoshmanobendra@gmail.com>
2026-02-23 09:42:41 +0530

    port fix

--- backend/main.py
+++ backend/main.py
@@ -377,4 +377,4 @@ async def upload_csv(
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=1000, reload=True)
